305bb4a2b689c85bc5a60ce0478903177d070131
telegram <telegram@nc.phobo.de>
2022-03-14 11:56:21 +0100

    before adding useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,27 @@ import initialNotes from "../shared/notes";
 import AddNoteForm from "../components/note/AddNoteForm";
 import { useState } from "react";
 
-console.log(initialNotes);
+/*
+const getInitialNotes = async (): Promise<Note[]> => {
+  console.log("getInitialNotes");
+  const uri = "http://localhost:3001/notes";
+  const res = await fetch(uri);
+  const notes = await res.json();
+  console.log("notes: ", notes);
+  return notes;
+};
+*/
 
 export default function Home() {
   const [notes, setNotes] = useState(initialNotes);
+
+  /*
+  useEffect(() => {
+    console.log("Only running on mount");
+    const initialNotes = await getInitialNotes();
+    setNotes(initialNotes);
+  }, []);
+*/
 
   function addNote(note: Note) {
     setNotes((prev) => {
@@ -16,8 +33,10 @@ export default function Home() {
   }
 
   function deleteNote(note: Note) {
-    console.log("deleting note ", note);
+    //console.log("deleting note ", note);
   }
+
+  //console.log("notes: ", notes);
 
   return (
     <>
